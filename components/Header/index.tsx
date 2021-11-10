75c234f4848960fdb371e30b69ec5ea30e129707
Nikolay Stadnik <nick.stdnk@gmail.com>
2021-11-11 02:28:43 +0300

    init

---
+++ components/Header/index.tsx
@@ -0,0 +1,59 @@
+import { FC } from 'react';
+import Link from 'next/link';
+import { Avatar, Box, Button, IconButton, Paper } from '@mui/material';
+import {
+  Menu as MenuIcon,
+  Search as SearchIcon,
+  SmsOutlined as MessageIcon,
+  NotificationsNoneOutlined as NotificationIcon,
+  KeyboardArrowDownOutlined as ArrowIcon,
+} from '@mui/icons-material';
+
+import styles from './Header.module.scss';
+
+export const Header: FC = () => {
+  return (
+    <Paper classes={{ root: styles.root }} elevation={0}>
+      <Box className="d-flex align-center">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <Link href="/">
+          <a>
+            <svg className={styles.logo} viewBox="0 0 24 25">
+              <path fill="#00FF00" d="M0 19h8.5v6H0v-6z"></path>
+              <path d="M0 7h8.5v18l6.5-6V7V0H8.5v7z"></path>
+              <path fill="rgba(0,0,0,0.15)" d="M7.5 19h1v6l-1-6z"></path>
+            </svg>
+          </a>
+        </Link>
+        <Box className={styles.searchBlock}>
+          <SearchIcon />
+          <input placeholder="Поиск" />
+        </Box>
+        <Button variant="contained" className={styles.createButton}>
+          Новая запись
+        </Button>
+      </Box>
+      <Box className="d-flex align-center">
+        <IconButton>
+          <MessageIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationIcon />
+        </IconButton>
+        <Link href="/profile/1">
+          <a className="d-flex align-center">
+            <Avatar
+              className={styles.avatar}
+              alt="Nikolay Stadnik"
+              src="https://leonardo.osnova.io/5d36a3a0-6e0b-5695-babd-3d72a73a30d8/-/scale_crop/108x108/-/format/webp/"
+            />
+          </a>
+        </Link>
+
+        <ArrowIcon />
+      </Box>
+    </Paper>
+  );
+};
---
+++ theme.ts
@@ -0,0 +1,63 @@
+import { createTheme } from "@mui/material";
+
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000'
+    }
+  },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+    MuiPaper:{
+      styleOverrides:{
+        rounded: {
+          borderRadius: 8
+        }
+      }
+    },
+    MuiDialog: {
+      styleOverrides:{
+        paper: {
+          boxShadow: 'none'
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: '8px',
+          textTransform: 'inherit',
+          fontSize: 16,
+          transition: 'none',
+          '&:active': {
+            boxShadow:
+              '0 1px 1px rgb(0 0 0 / 15%), 0 4px 7px rgb(0 0 0 / 0%), 0 -1px 0 rgb(0 0 0 / 5%), -1px 0 0 rgb(0 0 0 / 5%), 1px 0 0 rgb(0 0 0 / 5%) !important',
+            transform: 'translateY(1px)',
+          },
+        },  
+        contained: {
+          backgroundColor: 'white',
+          color: 'black',
+          boxShadow:
+            '0 1px 1px rgb(0 0 0 / 15%), 0 4px 7px rgb(0 0 0 / 5%), 0 -1px 0 rgb(0 0 0 / 5%), -1px 0 0 rgb(0 0 0 / 5%), 1px 0 0 rgb(0 0 0 / 5%)',
+          '&:hover': {
+            backgroundColor: 'white',
+            boxShadow:
+              '0 1px 1px rgb(0 0 0 / 18%), 0 4px 7px rgb(0 0 0 / 8%), 0 -1px 0 rgb(0 0 0 / 8%), -1px 0 0 rgb(0 0 0 / 8%), 1px 0 0 rgb(0 0 0 / 15%)',
+          },
+        }, 
+        containedPrimary: {
+          backgroundColor: '#4683d9',
+          '&:hover': {
+            backgroundColor: '#437CCE',
+          },
+        },   
+      }
+    },
+  }
+});
